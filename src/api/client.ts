d3edcd46713f20313c32b2b3a2f3409f3c4c0239
Lucas Passeli <lucaspasseli.work@gmail.com>
2023-05-18 02:24:30 -0300

    🎨 feat: added React Query and setup

---
+++ src/api/client.ts
@@ -0,0 +1,13 @@
+import { QueryClient } from 'react-query'
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 30000
+    }
+  }
+})
+
+export default client
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,20 @@ import App from './App'
 
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { QueryClientProvider } from 'react-query'
 
 import theme from './styles/theme'
 import GlobalStyle from './styles/globalStyles'
+import client from './api/client'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <GlobalStyle />
-        <App />
+        <QueryClientProvider client={client}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
